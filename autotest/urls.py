4485698d1d43eba6ff64e1fcdfdd7c6aff5c3786
pitt <956249457@qq.com>
2020-10-04 01:00:27 +0800

    2020-10-04 webcase commit

--- autotest/urls.py
+++ autotest/urls.py
@@ -20,6 +20,7 @@ from product import proviews
 from bug import bugviews
 from set import setviews
 from apptest import appviews
+from webtest import webviews
 
 
 urlpatterns = [
@@ -37,5 +38,7 @@ urlpatterns = [
     path('user/', setviews.set_user),
     path('test_report/',views.test_report),
     path('appcase_manage/', appviews.appcase_manage),
-    path('appcasestep_manage/', appviews.appcasestep_manage)
+    path('appcasestep_manage/', appviews.appcasestep_manage),
+    path('webcase_manage/', webviews.webcase_manage),
+    path('webcasestep_manage/', webviews.webcasestep_manage),
 ]
