f3e3b82e0dcb18e24efab6ad7b2420bc68ffe9c4
PITT <956249457@qq.com>
2021-11-09 18:50:16 +0800

    django改造,增加编辑、删除功能

--- apitest/views.py
+++ apitest/views.py
@@ -6,6 +6,7 @@ from apitest.models import Apitest,Apistep,Apis,Users
 from django.contrib.auth import authenticate, login
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, InvalidPage
 from django.views.decorators.clickjacking import xframe_options_sameorigin
+from django.views.generic import ListView
 import pymysql
 
 
@@ -34,42 +35,76 @@ def login(request):
 
 def logout(request):
     auth.logout(request)
-    return render(request,'login.html')
+    return render(request, 'login.html')
 
 
 #接口管理
 @login_required
 def apitest_manage(request):
     # 读取所有流程接口数据
-    apitest_list = Apitest.objects.all()
+    apitests_list = Apitest.objects.all()
+    apitests_count = apitests_list.count()
     # 浏览器登录session 
     username = request.session.get('user', '')
-    # 定义流程接口数据的变量并返回到前端
-    return render(request, "apitest_manage.html", {"user": username, "apitests": apitest_list})
+    apitest_list, left_has_more, right_has_more, left_page_range, right_page_range, paginator = getpagemessage(request,
+                                                                                                                apitests_list)
+
+    return render(request, "apitest_manage.html",
+                  {'user': username, "apitests": apitest_list, "left_has_more": left_has_more,
+                   "right_has_more": right_has_more, "left_page_range": left_page_range,
+                   "right_page_range": right_page_range, "paginator": paginator, "apitests_count":apitests_count})
+
 
 #接口步骤管理
 @login_required
 def apistep_manage(request):
     # 浏览器登录session
     username = request.session.get('user', '')
-    apistep_list = Apistep.objects.all()
-    # 定义流程接口数据的变量并返回到前端
-    return render(request, "apistep_manage.html", {"user": username, "apisteps": apistep_list})
+    apisteps_list = Apistep.objects.all()
+    apisteps_count = apisteps_list.count()
+    apisteps_list, left_has_more, right_has_more, left_page_range, right_page_range, paginator = getpagemessage(request,
+                                                                                                    apisteps_list)
+
+    return render(request, "apistep_manage.html",
+                  {'user': username, "apisteps": apisteps_list, "left_has_more": left_has_more,
+                   "right_has_more": right_has_more, "left_page_range": left_page_range,
+                   "right_page_range": right_page_range, "paginator": paginator,"apisteps_count":apisteps_count})
+
+
+def getpagemessage(request, message_list):
+    paginator = Paginator(message_list, 20)
+    page = request.GET.get('page', 1)
+    currentPage = int(page)
+    try:
+        message_list = paginator.page(page)
+    except PageNotAnInteger:
+        message_list = paginator.page(1)
+    except EmptyPage:
+        message_list = paginator.page(paginator.num_pages)
+    total = paginator.num_pages  # 总共的页码
+    around = 2
+    if currentPage <= around + 3:
+        left_has_more = False
+        left_page_range = range(1, currentPage)
+    else:
+        left_has_more = True
+        left_page_range = range(currentPage - around, currentPage)
+    if currentPage >= total - around - 2:
+        right_has_more = False
+        right_page_range = range(currentPage + 1, total + 1)
+    else:
+        right_has_more = True
+        right_page_range = range(currentPage + 1, currentPage + around + 1)
+    return message_list, left_has_more, right_has_more, left_page_range, right_page_range,paginator
 
 @login_required
 def apis_manage(request):
     username = request.session.get('user', '')
     apis_list = Apis.objects.all()
-    paginator = Paginator(apis_list, 20)
-    page = request.GET.get('page', 1)
-    currentPage=int(page)
-    try:
-        apis_list = paginator.page(page)
-    except PageNotAnInteger:
-        apis_list = paginator.page(1)
-    except EmptyPage:
-        apis_list = paginator.page(paginator.num_pages)
-    return render(request, "apis_manage.html", {'user': username, "apis": apis_list})
+    apis_account = apis_list.count()
+    apis_list, left_has_more, right_has_more, left_page_range, right_page_range, paginator = getpagemessage(request, apis_list)
+
+    return render(request, "apis_manage.html", {'user': username, "apis": apis_list, "left_has_more": left_has_more, "right_has_more": right_has_more, "left_page_range": left_page_range,"right_page_range": right_page_range, "paginator":paginator, "apis_account":apis_account })
 
 @login_required
 def users_manage(request):
@@ -97,10 +132,50 @@ def apistepsearch(request):
     username = request.session.get('user', '')
     search_title = request.GET.get("title", "")
     apistep_list = Apistep.objects.filter(title__contains=search_title)
-    return render(request, "apistep_manage.html", {"user": username, "apisteps": apistep_list})
+    return render(request, "apistep_manage.html", {"user": username, "apistep_list": apistep_list})
 
 
 @xframe_options_sameorigin
 def left(request):
     return render(request, "left.html")
 # Create your views here.
+
+class ApisView(ListView):
+    model = Apis
+    template_name = 'apis_manage.html'
+    context_object_name = 'apis_manage'
+    paginate_by = 10
+    ordering = '-id'
+    page_kwarg = 'page'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        paginator = context['paginator']
+        page_obj = context['page_obj']
+        context.update(self.get_pagination_data(paginator, page_obj))
+        return context
+
+    @staticmethod
+    def get_pagination_data(paginator, page_obj, around=2):
+        """这个分页算法的核心部分，其实仔细看看也不难"""
+        current = page_obj.number  # 当前页码
+        total = paginator.num_pages  # 总共的页码
+        if current <= around + 3:
+            left_has_more = False
+            left_page_range = range(1, current)
+        else:
+            left_has_more = True
+            left_page_range = range(current - around, current)
+        if current >= total - around - 2:
+            right_has_more = False
+            right_page_range = range(current + 1, total + 1)
+        else:
+            right_has_more = True
+            right_page_range = range(current + 1, current + around + 1)
+        return {
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'left_page_range': left_page_range,
+            'right_page_range': right_page_range
+        }
+
